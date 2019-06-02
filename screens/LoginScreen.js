7639a0f0cb01555d37745b70404538cc81286b68
Joona Viertola <contact@joonaviertola.com>
2019-06-02 15:48:37 +0300

    Added login screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, TextInput, Image, Button } from 'react-native';
+import { textInput, colorPumpkin, colorFire } from '../styles';
+import logo from '../assets/logo.png';
+
+export default class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit() {
+    return true;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={logo} />
+
+        <TextInput
+          style={styles.usernameField}
+          textContentType="username"
+          placeholder="Username"
+          onChangeText={text => this.setState({ username: text })}
+          value={this.state.username}
+        />
+
+        <TextInput
+          style={styles.passwordField}
+          textContentType="password"
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={text => this.setState({ password: text })}
+          value={this.state.password}
+        />
+
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this.onSubmit}
+            title="Hop in"
+            color={colorFire}
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colorPumpkin,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  usernameField: {
+    ...textInput,
+    marginTop: 25,
+    width: 200
+  },
+  passwordField: {
+    ...textInput,
+    marginTop: 20,
+    width: 200
+  },
+  buttonContainer: {
+    marginTop: 20
+  }
+});
---
+++ styles.js
@@ -0,0 +1,12 @@
+export const colorPumpkin = '#ff751a';
+export const colorFire = '#b34700';
+export const colorWhite = '#fff';
+
+export const textInput = {
+  borderBottomColor: colorWhite,
+  borderBottomWidth: 2,
+  fontSize: 20,
+  color: colorWhite,
+  paddingTop: 8,
+  paddingBottom: 8
+};
